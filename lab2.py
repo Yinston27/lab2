e384b4ecff2310093688ba48f7ef91f266c54009
Yinston <n3152u@gmail.com>
2025-11-13 14:15:59 +0300

    Внесены правки в названиях некоторых переменных и улучшена читаемость кода

--- lab2.py
+++ lab2.py
@@ -5,28 +5,29 @@ import xml.etree.ElementTree as ET
 
 def element_length_greater(count, list_dict_reader):
     return_list = []
-    for string in list_dict_reader:
-        if len(string['Название']) > count:
-            return_list.append(string)
-    for string in return_list:
-        print(string)
+    for entry in list_dict_reader:
+        if len(entry['Название']) > count:
+            return_list.append(entry)
+    for entry_return in return_list:
+        print(entry_return)
 
 
 def search_book(authors_name, list_dict_reader):
     return_list = []
-    for string in list_dict_reader:
-        if string['Автор'] == authors_name and int(string['Цена поступления']) >= 200:
-            return_list.append(string)
-    for string in return_list:
-        print(string)
+    for entry in list_dict_reader:
+        if entry['Автор'] == authors_name and int(entry['Цена поступления']) >= 200:
+            return_list.append(entry)
+    for entry_return in return_list:
+        print(entry_return)
 
 
-def link_generator(count_book, list):
+def link_generator(count_book, incoming_list):
     return_list = []
-    random_book = random.sample(list, len(list))
+    random_book = random.sample(incoming_list, len(incoming_list))
     for i in range(count_book):
-        #Делим "дата поступления" на две части: дату и время - берём только дату; делим дату по точкам и берём последний элемент - год
-        year = random_book[i]['Дата поступления'].split(' ')[0].split('.')[2]
+        #Делим "дата поступления" на две части: дату и время; делим дату по точкам и берём последний элемент - год
+        date_enrty, time_entry = random_book[i]['Дата поступления'].split(' ')
+        year = date_enrty.split('.')[-1]
         return_list.append(f'{i+1}. {random_book[i]['Автор']}. {random_book[i]['Название']} - {year}')
     with open("results.txt", 'w') as results:
         for i in range(count_book):
@@ -59,7 +60,7 @@ with open("books.csv") as books_file:
     books_reader = csv.DictReader(books_file, delimiter=";")
     books_list = [i for i in books_reader]  #список словарей, каждый словарь - строка исходной таблицы
 
-    # element_length_greater(30, books_list)
+    # element_length_greater(200, books_list)
     # search_book('Б. А. Воскресенский', books_list)
     # link_generator(20, books_list)
     # parser()
